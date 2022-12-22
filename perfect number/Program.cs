74e084ba6eb5fcee3923655625d482d068bb2744
abdelrahmanxd66 <121104890+abdelrahmanxd66@users.noreply.github.com>
2022-12-22 13:10:54 +0000

    Add files via upload

---
+++ perfect number/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace perfect_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("enter your first number");
+            double n1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter your second number");
+            double n2 = double.Parse(Console.ReadLine());
+
+            double sum = 0;
+
+            while (n1 < n2)
+            {
+                sum = 0;
+
+                int i;
+
+                for (i = 1; i < n1; i++)
+                {
+
+                    if (n1 % i == 0)
+                    {
+
+                        sum = sum + i;
+
+                    }
+
+                }
+
+
+                if (sum == n1)
+                {
+                    Console.WriteLine(n1);
+                }
+
+                n1++;
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ prime number/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prime_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("enter your first number");
+            float firstnumber = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("enter your second number");
+            long secondnumber = int.Parse(Console.ReadLine());
+
+
+            while (firstnumber < secondnumber)
+            {
+
+                int count = 0;
+
+
+                for (int i = 2; i < firstnumber; i++)
+                {
+
+
+                    if (firstnumber % i == 0)
+                    {
+
+                        count++;
+                    }
+
+
+                }
+
+
+
+                if (firstnumber < 2)
+                {
+
+                }
+
+                else if (count == 0)
+                {
+
+                    Console.WriteLine(firstnumber);
+                }
+
+
+                else if (count > 0)
+                {
+
+                }
+
+                firstnumber++;
+
+            }
+
+
+            System.Console.ReadKey();
+
+        }
+    }
+}
